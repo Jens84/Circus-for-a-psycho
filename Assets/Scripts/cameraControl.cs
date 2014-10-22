9ee9a16cb44bebbb5f41d78af12a75ccb8ab1398
Sigurd Knarhøi Johannsen <sigurd_johannsen@hotmail.com>
2014-10-22 21:21:00 +0200

    Updated the camera to smoothly follow the player vertically.

--- Assets/Scripts/cameraControl.cs
+++ Assets/Scripts/cameraControl.cs
@@ -2,12 +2,18 @@
 using System.Collections;
 
 public class cameraControl : MonoBehaviour {
-	public float speed;
+	public float speedHorizontal;				// horizontal camera speed
+	public float speedVertical;					// vertical camera speed
 	public static float cameraPositionX;		// send x-coordinate of screen to check if player lags behind
 												// this way we only kill player on one side of the screen
 
 	void FixedUpdate(){
-		transform.Translate(speed * Time.deltaTime, 0, 0);
+		Vector3 targetPosition = new Vector3 (transform.position.x,
+		                                    (characterController.characterPositionY + 0.5f), transform.position.z);
+
+		transform.Translate(speedHorizontal * Time.deltaTime, 0, 0);
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * speedVertical);
 	}
 
 	void Update(){
--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class characterController : MonoBehaviour {
 	public static bool playerDied = false;
+	public static float characterPositionY = 1.1f;
+
 	private bool facingRight = true;		// used to flip the character sprite
 
 	public float maxSpeed = 10f;
@@ -12,14 +14,14 @@ public class characterController : MonoBehaviour {
 	Animator anim;							// attaches animations to the character
 	
 	bool grounded = false;					// ground check for use in jumping
-	public static bool trampoline = false;	// check for use in jumping on trampolin
+	public static bool trampoline = false;	// check for use in jumping on trampoline
 	bool inScreen = true;					// is the character outside of camera bounds
-	public Transform groundCheck;			// in-game physics test for ground and trampolin
+	public Transform groundCheck;			// in-game physics test for ground and trampoline
 	public Transform character;				// in-game physics test for camera bounds
 	float groundRadius = 0.2f;
-	public LayerMask whatIsGround;
-	public LayerMask whatIsTrampoline;
-	public LayerMask whatIsScreenBorder;
+	public LayerMask whatIsGround;			// which gameObject counts as ground
+	public LayerMask whatIsTrampoline;		// which gameObject counts as trampoline
+	public LayerMask whatIsScreenBorder;	// bounding box of the camera - CURRENTLY NOT USED
 
 
 
@@ -34,18 +36,25 @@ public class characterController : MonoBehaviour {
 	void FixedUpdate () {
 		// check if character is grounded
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		// check if character is on top of trampoline
 		trampoline = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsTrampoline);
+		// check if character is on the screen - CURRENTLY NOT USED
 		inScreen = Physics2D.OverlapCircle (character.position, groundRadius, whatIsScreenBorder);
-		anim.SetBool ("Ground", grounded);
 
 		//if (grounded) {
 		//	doubleJumpUsed = false;
 		//}
-		// update vSpeed boolean for use in animation
-		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 
 		float move = Input.GetAxis ("Horizontal");
 
+		// ======================================
+		// UPDATING ANIMATION VARIABLES
+		// ======================================
+		// update Ground boolean for use in animation
+		anim.SetBool ("Ground", grounded);
+		// update vSpeed boolean for use in jump animation
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+		// update Speed boolean for use in run animation
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 
 		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
@@ -59,14 +68,17 @@ public class characterController : MonoBehaviour {
 		// ======================================
 		// PLAYER RESTART POSITION (CHECKPOINT)
 		// ======================================
+		// Later we should make a CHECKPOINT RESTART Vector3 for each checkpoint
+		// --------------------------------------
 		// player falls from the level
 		if (transform.position.y <= -4.0f) {
-			transform.position = new Vector3(-5.5f,1.1f, -2.0f);
+			transform.position = new Vector3(-6.0f,1.1f, -2.0f);
 			playerDied = true;
 		}
 	}
 
 	void Update () {
+		characterPositionY = transform.position.y;
 		// character will trampoline if on top of trampoline
 		if (trampoline) {
 			anim.SetBool ("Ground", false);
@@ -78,22 +90,29 @@ public class characterController : MonoBehaviour {
 			rigidbody2D.AddForce(new Vector2(0, jumpForce));
 		}
 		// ======================================
-		// CHECK IF PLAYER IS OUTSIDE SCREEN
+		// CHECK IF SCREEN CATCHES UP WITH PLAYER
 		// ======================================
 		if (!inScreen) {
 			if (transform.position.x < (cameraControl.cameraPositionX-13)){ // kills player if he lags behind
-				transform.position = new Vector3(-5.5f,1.1f, -2.0f);
+				transform.position = new Vector3(-6.0f,1.1f, -2.0f);
 				rigidbody2D.velocity = Vector2.zero;
 				playerDied = true;
 			}
-			//if (transform.position.x > (cameraControl.cameraPositionX+11)){
-			//	transform.position = new Vector3(cameraControl.cameraPositionX-11,
-			//	                                 transform.position.y, transform.position.z);
-			//}
+		// ======================================
+		// THIS FEATURE IS ACTUALLY REALLY COOL
+		// ======================================
+		// PLAYER EXITING SCREEN ON RIGHT ENTERS SCREEN ON LEFT
+		// --------------------------------------
+		//if (transform.position.x > (cameraControl.cameraPositionX+11)){
+		//	transform.position = new Vector3(cameraControl.cameraPositionX-11,
+		//	                                 transform.position.y, transform.position.z);
+		//}
 		}
 	}
 
-	// flips the sprite from left to right, when changing direction in game
+	// ======================================
+	// FLIP SPRITE ACCORDING TO PLAYER DIRECTION
+	// ======================================
 	void Flip(){
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
