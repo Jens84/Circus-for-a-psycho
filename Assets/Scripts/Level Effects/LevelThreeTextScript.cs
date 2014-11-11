d4f3cd69eda969549d03d81909776f22ccdc578f
Sigurd Knarhøi Johannsen <sigurd_johannsen@hotmail.com>
2014-11-11 14:45:46 +0100

    added introductory greeting to all 3 levels

---
+++ Assets/Scripts/Level Effects/LevelThreeTextScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelThreeTextScript : MonoBehaviour {
+	
+	public AudioClip EscapeTheCircusSound;
+	
+	void Awake () {
+		audio.PlayOneShot(EscapeTheCircusSound);
+		Destroy (gameObject, 2.0f);
+	}
+}
---
+++ Assets/Scripts/Level Effects/LevelTwoTextScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTwoTextScript : MonoBehaviour {
+	
+	public AudioClip FreeTheAnimalsSound;
+	
+	void Awake () {
+		audio.PlayOneShot(FreeTheAnimalsSound);
+		Destroy (gameObject, 2.0f);
+	}
+}
