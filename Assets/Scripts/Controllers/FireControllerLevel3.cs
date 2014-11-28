8edefdb207894d1ed1f1e7ffa1ca5eae43792806
Sigurd Knarhøi Johannsen <sigurd_johannsen@hotmail.com>
2014-11-28 23:48:58 +0100

    finished lv3

--- Assets/Scripts/Controllers/FireControllerLevel3.cs
+++ Assets/Scripts/Controllers/FireControllerLevel3.cs
@@ -3,46 +3,39 @@ using System.Collections;
 
 public class FireControllerLevel3 : MonoBehaviour, IPlayerRespawnListener
 {
-    public float FireSpeed = 5;
+	public float FireSpeed = 5;
 
-    private Player _player;
-    private Vector3 _startPosition;
-    private bool _increased = false;
+	private Player _player;
+	private Vector3 _startPosition;
 
-    public void Awake()
-    {
-        _player = GameObject.FindObjectOfType<Player>();
-        _startPosition = transform.position;
-    }
+	public void Awake()
+	{
+		_player = GameObject.FindObjectOfType<Player>();
+		_startPosition = transform.position;
+	}
 
-    public void FixedUpdate()
-    {
-        if (_player.IsDead)
-        {
-            transform.position = _startPosition;
-            //gameObject.SetActive(false);
-            return;
-        }
+	public void FixedUpdate () {
+		if (_player.IsDead) {
+			transform.position = _startPosition;
+			//gameObject.SetActive(false);
+			return;
+		}
 
-        if (transform.position.y <= 200)
-        {
-            transform.Translate(0, FireSpeed * Time.deltaTime, 0);
-        }
+		if (transform.position.y <= 205)
+		{
+			if (transform.position.y > 100)
+				transform.Translate(0, 0.6f * FireSpeed * Time.deltaTime, 0);
+			else
+				transform.Translate(0, FireSpeed * Time.deltaTime, 0);
+		}
+	}
 
-        if (_player.transform.position.y > 40 && !_increased)
-        {
-            FireSpeed += 1;
-            _increased = true;
-        }
-
-    }
-
-    // From IPlayerRespawnListener, to respond when the player get reInstantiated
-    public void OnPlayerRespawnInThisCheckPoint(Checkpoint2D checkpoint, Player player)
-    {
-        transform.localScale = new Vector3(1, 1, 1);
-        float toY = checkpoint.transform.position.y - 20;
-        transform.position = new Vector3(_startPosition.x, toY, _startPosition.z);
-        //gameObject.SetActive(true);
-    }
+	// From IPlayerRespawnListener, to respond when the player get reInstantiated
+	public void OnPlayerRespawnInThisCheckPoint(Checkpoint2D checkpoint, Player player)
+	{
+		transform.localScale = new Vector3(1, 1, 1);
+		float toY = checkpoint.transform.position.y - 20;
+		transform.position = new Vector3(_startPosition.x, toY, _startPosition.z);
+		//gameObject.SetActive(true);
+	}
 }
---
+++ Assets/Scripts/Platforms/PlusChanger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlusChanger : MonoBehaviour
+{
+	bool activated = true;
+	// set A of objects
+	GameObject RedPlus;
+
+	// set B of objects
+	GameObject GreyPlus;
+	
+	void Start()
+	{
+		// set A of objects
+		RedPlus = GameObject.FindWithTag("RedPlus");
+		// set B of objects
+		GreyPlus = GameObject.FindWithTag("GreyPlus");
+	}
+	
+	void Update()
+	{
+		if (activated && CharacterController2D.PlayerJumped)
+		{
+			CharacterController2D.PlayerJumped = false;
+			// set A of objects
+			RedPlus.SetActive(false);
+			// set B of objects
+			GreyPlus.SetActive(true);
+			
+			activated = !activated;
+		}
+		if (!activated && CharacterController2D.PlayerJumped)
+		{
+			CharacterController2D.PlayerJumped = false;
+			// set A of objects
+			RedPlus.SetActive(true);
+			// set B of objects
+			GreyPlus.SetActive(false);
+			
+			activated = !activated;
+		}
+	}
+}
