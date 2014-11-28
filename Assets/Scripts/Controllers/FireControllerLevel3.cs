4929ffec004b97a648e661b2445e6b64a2e5c7bb
Eleftherios Manousakis <lemanousakis@gmail.com>
2014-11-28 21:10:49 +0100

    Level3 updates(fire)

--- Assets/Scripts/Controllers/FireControllerLevel3.cs
+++ Assets/Scripts/Controllers/FireControllerLevel3.cs
@@ -3,36 +3,46 @@ using System.Collections;
 
 public class FireControllerLevel3 : MonoBehaviour, IPlayerRespawnListener
 {
-	public float FireSpeed = 5;
+    public float FireSpeed = 5;
 
-	private Player _player;
-	private Vector3 _startPosition;
+    private Player _player;
+    private Vector3 _startPosition;
+    private bool _increased = false;
 
-	public void Awake()
-	{
-		_player = GameObject.FindObjectOfType<Player>();
-		_startPosition = transform.position;
-	}
+    public void Awake()
+    {
+        _player = GameObject.FindObjectOfType<Player>();
+        _startPosition = transform.position;
+    }
 
-	public void FixedUpdate () {
-		if (_player.IsDead) {
-			transform.position = _startPosition;
-			//gameObject.SetActive(false);
-			return;
-		}
+    public void FixedUpdate()
+    {
+        if (_player.IsDead)
+        {
+            transform.position = _startPosition;
+            //gameObject.SetActive(false);
+            return;
+        }
 
-		if (transform.position.y <= 200)
-		{
-			transform.Translate(0, FireSpeed * Time.deltaTime, 0);
-		}
-	}
+        if (transform.position.y <= 200)
+        {
+            transform.Translate(0, FireSpeed * Time.deltaTime, 0);
+        }
 
-	// From IPlayerRespawnListener, to respond when the player get reInstantiated
-	public void OnPlayerRespawnInThisCheckPoint(Checkpoint2D checkpoint, Player player)
-	{
-		transform.localScale = new Vector3(1, 1, 1);
-		float toY = checkpoint.transform.position.y - 20;
-		transform.position = new Vector3(_startPosition.x, toY, _startPosition.z);
-		//gameObject.SetActive(true);
-	}
+        if (_player.transform.position.y > 40 && !_increased)
+        {
+            FireSpeed += 1;
+            _increased = true;
+        }
+
+    }
+
+    // From IPlayerRespawnListener, to respond when the player get reInstantiated
+    public void OnPlayerRespawnInThisCheckPoint(Checkpoint2D checkpoint, Player player)
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+        float toY = checkpoint.transform.position.y - 20;
+        transform.position = new Vector3(_startPosition.x, toY, _startPosition.z);
+        //gameObject.SetActive(true);
+    }
 }
--- Assets/Scripts/Level Effects/GiveDamageToPlayer.cs
+++ Assets/Scripts/Level Effects/GiveDamageToPlayer.cs
@@ -14,7 +14,7 @@ public class GiveDamageToPlayer : MonoBehaviour
         _lastPosition = transform.position;
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerStay2D(Collider2D other)
     {
         var player = other.GetComponent<Player>();  // To have this object interact with objects that have the player scipt attached
         if (player == null)                         // Skip for other than player objects
