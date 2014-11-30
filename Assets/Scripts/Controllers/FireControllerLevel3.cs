36abccc2a93cf543f1d7708174f59e8ee8ab0ca4
Eleftherios Manousakis <lemanousakis@gmail.com>
2014-11-30 17:42:49 +0100

    Polishing levels

--- Assets/Scripts/Controllers/FireControllerLevel3.cs
+++ Assets/Scripts/Controllers/FireControllerLevel3.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class FireControllerLevel3 : MonoBehaviour
 {
-    public float FireSpeed = 5;
+    public float FireSpeed = 2;
 
     private Player _player;
     private Vector3 _startPosition;
@@ -16,19 +16,28 @@ public class FireControllerLevel3 : MonoBehaviour
         _startPosition.x = transform.position.x;
     }
 
-    public void FixedUpdate()
+    public void Update()
     {
         if (_player.IsRespawning)
         {
             transform.localScale = new Vector3(1, 1, 1);
             float toY = _player.transform.position.y;
-            transform.position = new Vector3(_startPosition.x, toY, _startPosition.z);        
+            transform.position = new Vector3(_startPosition.x, toY, _startPosition.z);
         }
+    }
 
-        if (transform.position.y <= 205)
+    public void FixedUpdate()
+    {
+        if (_player.IsDead)
+        {
+            transform.Translate(0, 0, 0);
+        }
+        else if (transform.position.y <= 205) // Top
         {
             if (transform.position.y > 100)
-                transform.Translate(0, 0.6f * FireSpeed * Time.deltaTime, 0);
+                transform.Translate(0, 0.7f * FireSpeed * Time.deltaTime, 0);
+            else if (transform.position.y > 30 && transform.position.y < 80)
+                transform.Translate(0, 1.6f * FireSpeed * Time.deltaTime, 0);
             else
                 transform.Translate(0, FireSpeed * Time.deltaTime, 0);
         }
